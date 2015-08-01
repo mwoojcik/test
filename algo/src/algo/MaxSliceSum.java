59b388db0c36993783dcee0319b36f8223d36227
Marek <Marek@MAREK-ASUS>
2015-08-01 12:17:40 +0200

    tets

--- algo/src/algo/MaxSliceSum.java
+++ algo/src/algo/MaxSliceSum.java
@@ -3,13 +3,10 @@ package algo;
 public class MaxSliceSum {
 
 	public static void main(String[] args) {
-		
-		int[] A = new int[] {3, 2, -6, 4, 0}; //5	
-		int[] B = new int[] {1, 0, 2, -1, 0, 1, 3};	//6
+
 		int[] C = new int[] {1, 0, 2, -1, -2, 0, 1, 3};	// 4
-		int[] D = new int[] {-1, 3, -3,-1,5}; //1
-		
-		System.out.println(solution(D));
+
+		System.out.println(solution(C));
 	}
 
 	public static int solution(int[] A) {
