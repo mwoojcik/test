da83ec4edf5b22c5329755f3e1bd57a66a0bbcc4
Marek <Marek@MAREK-ASUS>
2015-08-01 12:30:00 +0200

    gh

--- algo/src/algo/MaxSliceSum.java
+++ algo/src/algo/MaxSliceSum.java
@@ -3,10 +3,10 @@ package algo;
 public class MaxSliceSum {
 
 	public static void main(String[] args) {
-
-		int[] C = new int[] {1, 0, 2, -1, -2, 0, 1, 3};	// 4
-
-		System.out.println(solution(C));
+		
+		int[] c = new int[] {-1, 3, -3,-1,5}; //1
+		
+		System.out.println(solution(c));
 	}
 
 	public static int solution(int[] A) {
